ad28491838bbcc94cffbcd3b348b8371e6079c96
David <davidmur@gmail.com>
2019-08-17 01:22:26 +0200

    cambio en get usuario

--- server/server.js
+++ server/server.js
@@ -14,7 +14,7 @@ app.use(bodyParser.json())
 
 
 app.get('/usuario', function(req, res) {
-    res.json('get Usuario')
+    res.json('get Usuario ');
 });
 
 app.post('/usuario', function(req, res) {
