d55e6d9d7535941573c7bc7e738ac67ea5a2a06b
David <davidmur@gmail.com>
2020-08-07 08:53:12 +0200

    RestServer - usuario.Se modificó url de mongodb

--- server/config/config.js
+++ server/config/config.js
@@ -20,7 +20,8 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else{
-    urlDB = 'mongodb://cafe-user:12345@cluster0-hi6dz.mongodb.net:27017/cafe';
+    //urlDB = 'mongodb://cafe-user:12345@cluster0-hi6dz.mongodb.net:27017/cafe';
+    urlDB = 'mongodb+srv://cafe-user:12345@cluster0-hi6dz.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 
 process.env.urlDB = urlDB;
