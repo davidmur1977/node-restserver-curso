1a6aac1f18bf43fbf7434554547994d614966a92
David <davidmur@gmail.com>
2020-08-07 08:06:16 +0200

    RestServer - usuario 07/08/2020

--- server/config/config.js
+++ server/config/config.js
@@ -17,10 +17,10 @@ process.env.NODE_ENV = process.env.NODE_ENV || 'dev';
 //=================================
 let urlDB;
 
-//if (process.env.NODE_ENV === 'dev') {
-//    urlDB = 'mongodb://localhost:27017/cafe';
-//} else{
+if (process.env.NODE_ENV === 'dev') {
+    urlDB = 'mongodb://localhost:27017/cafe';
+} else{
     urlDB = 'mongodb://cafe-user:12345@cluster0-hi6dz.mongodb.net:27017/cafe';
-//}
+}
 
 process.env.urlDB = urlDB;
