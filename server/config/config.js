7a84b2d4a8c813c73cd0f1b7ba02524873947ad5
David <davidmur@gmail.com>
2020-08-09 22:09:36 +0200

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -21,7 +21,7 @@ if (process.env.NODE_ENV === 'dev') {
     urlDB = 'mongodb://localhost:27017/cafe';
 } else{
     //urlDB = 'mongodb://cafe-user:12345@cluster0-hi6dz.mongodb.net:27017/cafe';
-    urlDB = 'mongodb+srv://cafe-user:12345@cluster0-hi6dz.mongodb.net/cafe?retryWrites=true&w=majority';
+    urlDB = process.env.MONGO_URI;
     //urlDB = 'mongodb+srv://adminDavid:z2SNtcdfp2A90uGw@cluster0-hi6dz.mongodb.net/cafe?retryWrites=true&w=majority';
 }
 
